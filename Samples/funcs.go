6462ecd0ab976733a4aea208006acab3443bcab2
Yutaro Suzuki <yutaro1985@gmail.com>
2020-01-20 04:11:19 +0900

    152_Cの内容に変更

--- Samples/funcs.go
+++ Samples/funcs.go
@@ -5,20 +5,35 @@ import (
 	"fmt"
 	"os"
 	"strconv"
-	"strings"
 )
-
-// このソース自体は多分動かない。
 
 const (
-	// 初期バッファサイズ
 	initialBufSize = 10000
-	// バッファサイズの最大値。Scannerは必要に応じこのサイズまでバッファを大きくして各行をスキャンする。
-	// この値がinitialBufSize以下の場合、Scannerはバッファの拡張を一切行わず与えられた初期バッファのみを使う。
-	maxBufSize = 1000000
+	maxBufSize     = 1000000009
 )
 
+var buf []byte = make([]byte, maxBufSize)
 var sc = bufio.NewScanner(os.Stdin)
+
+func init() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(buf, maxBufSize)
+}
+
+func main() {
+	nums := nextInt()
+	trueTotal := 0
+	min := 1000000
+	var cur int
+	for i := 0; i < nums; i++ {
+		cur = nextInt()
+		if min >= cur {
+			trueTotal++
+			min = cur
+		}
+	}
+	fmt.Println(trueTotal)
+}
 
 func nextLine() string {
 	sc.Scan()
@@ -34,49 +49,16 @@ func nextInt() int {
 	return i
 }
 
-var rdr = bufio.NewReaderSize(os.Stdin, 1000000)
-
-func readLine() string {
-	buf := make([]byte, 0, 1000000)
-	for {
-		l, p, e := rdr.ReadLine()
-		if e != nil {
-			panic(e)
-		}
-		buf = append(buf, l...)
-		if !p {
-			break
-		}
-	}
-	return string(buf)
+func readInt64() int64 {
+	sc.Scan()
+	r, _ := strconv.ParseInt(sc.Text(), 10, 64)
+	return r
 }
 
-func main() {
-	var s1, s2 string
-	s1 = nextLine()
-	s2 = nextLine()
-	nums, _ := strconv.Atoi(s1)
-	slice := strings.Split(s2, " ")
-	inputNum := stringToint(slice)
-
-	truenums := 0
-	min := inputNum[0]
-	for i := 0; i < nums; i++ {
-		if min >= inputNum[i] {
-			min = inputNum[i]
-			truenums++
-		}
-	}
-	fmt.Println(truenums)
-}
-func Scanner() []string {
-	sc := bufio.NewScanner(os.Stdin)
-	var s string
-	if sc.Scan() {
-		s = sc.Text()
-	}
-	slice := strings.Split(s, " ")
-	return slice
+func readFloat64() float64 {
+	sc.Scan()
+	r, _ := strconv.ParseFloat(sc.Text(), 64)
+	return r
 }
 
 func stringToint(s []string) []int {
@@ -85,25 +67,4 @@ func stringToint(s []string) []int {
 		f[n], _ = strconv.Atoi(s[n])
 	}
 	return f
-}
-
-// 一つずつ読み込んで比較するのでもしかすると時間かかるかも
-func max(a []int) int {
-	max := a[0]
-	for _, i := range a {
-		if i > max {
-			max = i
-		}
-	}
-	return max
-}
-
-func min(a []int) int {
-	min := a[0]
-	for _, i := range a {
-		if i < min {
-			min = i
-		}
-	}
-	return min
 }
