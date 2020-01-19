504214ec903acf2ead46b102b9654e2b8bfeba45
Yutaro Suzuki <yutaro1985@gmail.com>
2020-01-19 23:13:27 +0900

    個人的に素晴らしい実装だと思ったのでメモとして

---
+++ AtCoder/ABC/152/C_ref.go
@@ -0,0 +1,22 @@
+_package main
+ 
+import (
+	"fmt"
+)
+ 
+func main() {
+	var n int
+	fmt.Scan(&n)
+	var cnt int
+	min := int(2e5 + 1)
+	for i := 0; i < n; i++ {
+		var p int
+		fmt.Scan(&p)
+      fmt.Println(p)
+		if p <= min {
+			cnt++
+			min = p
+		}
+	}
+	fmt.Println(cnt)
+}
