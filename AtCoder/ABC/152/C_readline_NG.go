83af9ce07087d62c4a6fe90b4cfb374637ef1f72
Yutaro Suzuki <yutaro1985@gmail.com>
2020-01-20 01:49:20 +0900

    readlineパターン（失敗）

---
+++ AtCoder/ABC/152/C_readline_NG.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var rdr = bufio.NewReaderSize(os.Stdin, 1000000000)
+
+func readLine() string {
+	buf := make([]byte, 0, 1000000000)
+	for {
+		l, p, e := rdr.ReadLine()
+		if e != nil {
+			panic(e)
+		}
+		buf = append(buf, l...)
+		if !p {
+			break
+		}
+	}
+	return string(buf)
+}
+
+func main() {
+	s1 := readLine()
+	s2 := readLine()
+	s2col := strings.Split(s2, " ")
+	nums, _ := strconv.Atoi(s1)
+	trueTotal := 0
+	min := 2 * 10000
+	for i := 0; i < nums; i++ {
+		num, _ := strconv.Atoi(s2col[i])
+		if min > num {
+			trueTotal++
+			min = num
+		}
+	}
+	fmt.Println(trueTotal)
+}
