d17d99b333d76b0da70738ee233bf67700553d46
Yutaro Suzuki <yutaro1985@gmail.com>
2020-01-21 03:19:22 +0900

    一旦ここまで。なぜWAになるのか検証できない…

--- AtCoder/ABS/ABC085C_On.go
+++ AtCoder/ABS/ABC085C_On.go
@@ -1,6 +1,12 @@
 // こちらの方法をGoで実装してみる
 //https://ikatakos.com/pot/programming_algorithm/contest_history/atcoder/2018/0107_abc085
+//https://akhtikd.com/posts/atcoder-beginners-selection/
 // O(N)のパターン。鶴亀算の要領
+//58,000=10000∗i+5000∗j+1000∗k⋯(1)
+// 9=i+j+k⋯(2)
+// 9 - (i+j) = k
+//49000(rest) = 9000*i + 4000*j
+// ↑の式でiを一つずつ変えればjも決まる
 // 1問(b10)だけうまく行かず…。
 
 package main
@@ -17,7 +23,7 @@ func main() {
 	rest := y - tmp1000
 	for i := 0; i <= rest/4000; i++ {
 		tmp5000 := i * 4000
-		// iが5000円の枚数
+		// ここではiが5000円の枚数
 		if rest-tmp5000 < 0 {
 			break
 		}
