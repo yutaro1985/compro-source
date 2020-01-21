e6f4b595a0dc4b7e1e9d65310ceafd112aebb641
Yutaro Suzuki <yutaro1985@gmail.com>
2020-01-22 03:39:35 +0900

    メモ追記

---
+++ AtCoder/ABS/ABC049C_others.go
@@ -0,0 +1,9 @@
+// TODO アルゴリズムの勉強のため後々まだ実装してない他の方法でも実装します。
+// ・Greedyアルゴリズム
+// https://qiita.com/drken/items/fd4e5e3630d0f5859067#%E7%AC%AC-9-%E5%95%8F--abc-049-c---daydream-300-%E7%82%B9
+//
+// ・DP（動的計画法）
+// ・幅優先探索
+// ・ちょっと先まで見る
+// ・深さ優先探索
+// https://qiita.com/259_Momone/items/991d31ccc1f830a1d578#%E7%AC%AC-9-%E5%95%8F--abc049c---%E7%99%BD%E6%98%BC%E5%A4%A2--daydream
