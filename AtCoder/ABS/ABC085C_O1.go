781f3347ec9fd477c9f7ca90596e2f1c16fc2289
Yutaro Suzuki <yutaro1985@gmail.com>
2020-01-22 01:00:33 +0900

    TODO O(1)のパターンを後で実装する

---
+++ AtCoder/ABS/ABC085C_O1.go
@@ -0,0 +1,14 @@
+// こちらの方法をGoで実装してみる
+//https://ikatakos.com/pot/programming_algorithm/contest_history/atcoder/2018/0107_abc085
+// O(1)のパターン
+// TODO
+
+package main
+
+import "fmt"
+
+func main() {
+	var n, y int
+	fmt.Scan(&n, &y)
+
+}
