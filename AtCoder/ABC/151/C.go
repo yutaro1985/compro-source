1471273e938a1d2ec317bcc2b54c6f484d2ab49e
yutaro1985 <yutaro.suzuki@cloudys.dev>
2020-01-24 19:39:43 +0900

    一旦コミット。後で追加する

---
+++ AtCoder/ABC/151/C.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 10000
+	maxBufSize     = 1000000009
+)
+
+var buf []byte = make([]byte, maxBufSize)
+var sc = bufio.NewScanner(os.Stdin)
+
+func init() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(buf, maxBufSize)
+}
+
+func main() {
+	n := nextInt()
+	m := nextInt()
+	// cleared :=make()
+	for i := 0; i < m; i++ {
+		p := nextInt()
+		s := nextLine()
+	}
+}
+
+func nextLine() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func stringToint(s []string) []int {
+	f := make([]int, len(s))
+	for n := range s {
+		f[n], _ = strconv.Atoi(s[n])
+	}
+	return f
+}
+
