c1c723bd95ab2d45a3253912cd351190ec033afa
Yutaro Suzuki <yutaro1985@gmail.com>
2020-02-04 01:44:48 +0900

    ABC153C AC

---
+++ AtCoder/ABC/153/B.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 10000
+	maxBufSize     = 1000000009
+)
+
+var buf []byte = make([]byte, maxBufSize)
+var sc = bufio.NewScanner(os.Stdin)
+
+func init() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(buf, maxBufSize)
+}
+
+func main() {
+	h := nextInt()
+	n := nextInt()
+	var totalA int
+	for i := 0; i < n; i++ {
+		totalA += nextInt()
+	}
+	if h <= totalA {
+		fmt.Println("Yes")
+	} else {
+		fmt.Println("No")
+	}
+}
+
+func nextInt() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
