d9a5e7032a62f82083579ddfb143323f6a72d2e0
yutaro1985 <yutaro.suzuki@cloudys.dev>
2020-03-03 14:38:10 +0900

    計算量が多すぎる、かつWAが出る

---
+++ AtCoder/ABC/127/C/C_TLE.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 10000
+	maxBufSize     = 1000000009
+)
+
+var buf []byte = make([]byte, maxBufSize)
+var sc = bufio.NewScanner(os.Stdin)
+
+func init() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(buf, maxBufSize)
+}
+
+func main() {
+	N := nextInt()
+	M := nextInt()
+	m := make(map[int]int)
+	keys := make([]int, 0, M)
+	pass := make([]int, N)
+	maxcount := 0
+	tmp := 0
+	ans := 0
+	for i := 0; i < M; i++ {
+		L := nextInt()
+		m[L] = nextInt()
+	}
+	for k := range m {
+		keys = append(keys, k)
+	}
+	// sort.Ints(keys)
+	// fmt.Println(keys, m)
+	for row, L := range keys {
+		for i := L - 1; i <= m[L]-1; i++ {
+			// fmt.Println(L, i)
+			if pass[i] == maxcount {
+				pass[i]++
+				tmp = pass[i]
+				if row == len(keys)-1 {
+					ans++
+				}
+			}
+		}
+		maxcount = tmp
+	}
+	fmt.Println(ans)
+}
+
+func nextInt() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
