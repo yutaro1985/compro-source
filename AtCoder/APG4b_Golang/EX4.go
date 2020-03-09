84fcd8dfc8b8e64e01b849e901dadf608dcb51a9
yutaro1985 <yutaro.suzuki@cloudys.dev>
2020-03-09 19:54:14 +0900

    APG4bのやった分

---
+++ AtCoder/APG4b_Golang/EX4.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+const seconds = 365 * 24 * 60 * 60
+
+func main() {
+	fmt.Println(seconds)
+	fmt.Println(seconds * 2)
+	fmt.Println(seconds * 5)
+	fmt.Println(seconds * 10)
+}
