e93a4d08d8c336184cb20cc4736f442207a8d12c
Yutaro Suzuki <yutaro1985@gmail.com>
2020-03-11 04:26:48 +0900

    WA。後でやり直す

---
+++ AtCoder/ABC/ABC_158/C/C.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var A, B int
+	fmt.Scan(&A, &B)
+	low8 := int(float64(A) / 0.08)
+	high8 := int((float64(A) + 0.9) / 0.08)
+	low10 := int(float64(B) / 0.1)
+	high10 := int((float64(B) + 0.9) / 0.1)
+	if high8 < low10 || high10 < low8 {
+		fmt.Println(-1)
+	} else if low8 < low10 {
+		fmt.Println(high8)
+	} else {
+		fmt.Println(low8)
+	}
+	fmt.Println(low8, high8, low10, high10)
+}
