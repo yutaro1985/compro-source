0b20105b2d412245d5e577ed7c8652566fb9a217
Yutaro Suzuki <yutaro1985@gmail.com>
2020-03-11 03:48:40 +0900

    検証用関数追加

---
+++ test/test5.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println(int(1000000000000000000.0))
+}
+
+// 下記を検証する
+func min(nums ...int) int {
+	if len(nums) == 0 {
+		panic("funciton min() requires at least one argument.")
+	}
+	res := nums[0]
+	for i := 0; i < len(nums); i++ {
+		res = int(math.Min(float64(res), float64(nums[i])))
+	}
+	return res
+}
+
+func max(nums ...int) int {
+	if len(nums) == 0 {
+		panic("funciton max() requires at least one argument.")
+	}
+	res := nums[0]
+	for i := 0; i < len(nums); i++ {
+		res = int(math.Max(float64(res), float64(nums[i])))
+	}
+	return res
+}
