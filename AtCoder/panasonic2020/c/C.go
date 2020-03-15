ae0068960ef3603ecef9fc81e85004efa421ce20
Yutaro Suzuki <yutaro1985@gmail.com>
2020-03-16 02:51:40 +0900

    補足追記

--- AtCoder/panasonic2020/c/C.go
+++ AtCoder/panasonic2020/c/C.go
@@ -5,6 +5,8 @@ import "fmt"
 func main() {
 	var a, b, c int
 	fmt.Scan(&a, &b, &c)
+	// 誤差により小数同士の比較は想定通りにならないケースがあるため、条件を整数で比較する形にする
+	// c-a-bがマイナスの場合は不等号の向きが変わってしまうので場合分けが必要だが、今回のケースだと成立しないケースしかないのでNo
 	condition := (c-a-b)*(c-a-b) - 4*a*b
 	if condition > 0 && c > a+b {
 		fmt.Println("Yes")
