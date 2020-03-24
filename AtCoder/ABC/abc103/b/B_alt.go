79197353eb2bf8fdca38b4b9e27e54aa474292a2
yutaro1985 <yutaro.suzuki@cloudys.dev>
2020-03-24 15:27:18 +0900

    srtingを扱う配列の指定の仕方を利用した、もう少しスマートな書き方

---
+++ AtCoder/ABC/abc103/b/B_alt.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var S, T string
+	fmt.Scan(&S, &T)
+	for i := 0; i < len(S); i++ {
+		fmt.Println(S[i:] + S[:i])
+		if T == S[i:]+S[:i] {
+			fmt.Println("Yes")
+			return
+		}
+	}
+	fmt.Println("No")
+}
