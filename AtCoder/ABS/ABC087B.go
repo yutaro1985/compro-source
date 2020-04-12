9971fbb2e6b0df0ab40f7fbbbceed0b88ddb7f63
Yutaro Suzuki <yutaro1985@gmail.com>
2020-04-12 10:28:02 +0900

    見づらくなってたので関数に分離

--- AtCoder/ABS/ABC087B.go
+++ AtCoder/ABS/ABC087B.go
@@ -16,32 +16,44 @@ func main() {
 		}
 		// 100円玉を最も多く使った払い方と、最も少なく使った払い方を求める
 		var maxh, minf int
-		if rest >= 100*b {
-			maxh = b
-			rest -= 100 * b
-		} else {
-			maxh = rest / 100
-			rest %= 100
-		}
-		if rest <= c*50 && rest%50 == 0 {
-			minf = rest / 50
-		}
-		rest = x - 500*i
+		maxh, minf = max100ycoins(rest, b, c)
 		var minh, maxf int
-		if rest >= 50*c {
-			if (rest-50*c)%100 != 0 {
-				maxf = c - 1
-			} else {
-				maxf = c
-			}
-			rest -= 50 * maxf
-			minh = rest / 100
-		} else {
-			maxf = rest / 50
-		}
+		minh, maxf = min100ycoins(rest, b, c)
+
 		if 500*i+100*maxh+50*minf == x && 500*i+100*minh+50*maxf == x {
 			ans += maxh - minh + 1
 		}
 	}
 	fmt.Println(ans)
+}
+
+func max100ycoins(rest int, b int, c int) (int, int) {
+	var maxh, minf int
+	if rest >= 100*b {
+		maxh = b
+		rest -= 100 * b
+	} else {
+		maxh = rest / 100
+		rest %= 100
+	}
+	if rest <= c*50 && rest%50 == 0 {
+		minf = rest / 50
+	}
+	return maxh, minf
+}
+
+func min100ycoins(rest int, b int, c int) (int, int) {
+	var minh, maxf int
+	if rest >= 50*c {
+		if (rest-50*c)%100 != 0 {
+			maxf = c - 1
+		} else {
+			maxf = c
+		}
+		rest -= 50 * maxf
+		minh = rest / 100
+	} else {
+		maxf = rest / 50
+	}
+	return minh, maxf
 }
