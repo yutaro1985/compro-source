29b60f521d164d282d5659b4687609587a28cb90
Yutaro Suzuki <yutaro1985@gmail.com>
2020-04-24 02:04:05 +0900

    abc046 A,B AC 問題は読もう

---
+++ AtCoder/ABC/abc046/a/A.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 1e4
+	maxBufSize     = 1e6 + 7
+)
+
+var buf []byte = make([]byte, maxBufSize)
+var sc = bufio.NewScanner(os.Stdin)
+
+func init() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(buf, maxBufSize)
+}
+
+func main() {
+	a, b, c := nextInt(), nextInt(), nextInt()
+	ans := 2
+	if a == b && b == c {
+		ans = 1
+	}
+	if a != b && b != c && c != a {
+		ans = 3
+	}
+	fmt.Println(ans)
+}
+
+func nextInt() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
---
+++ AtCoder/ABC/abc046/b/B.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 1e4
+	maxBufSize     = 1e6 + 7
+)
+
+var buf []byte = make([]byte, maxBufSize)
+var sc = bufio.NewScanner(os.Stdin)
+
+func init() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(buf, maxBufSize)
+}
+
+func main() {
+	N, K := nextInt(), nextInt()
+	fmt.Println(K * int(math.Pow(float64(K-1), float64(N-1))))
+}
+
+func nextInt() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
