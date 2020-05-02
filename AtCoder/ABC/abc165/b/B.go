2f90b038b777b0d1ca33a54e0db11c67c69bab54
Yutaro Suzuki <yutaro1985@gmail.com>
2020-05-03 00:36:22 +0900

    ABC165 A,B,D AC Cは難しかった

---
+++ AtCoder/ABC/abc165/b/B.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 1e4
+	maxBufSize     = 1e6 + 7
+)
+
+var buf []byte = make([]byte, maxBufSize)
+var sc = bufio.NewScanner(os.Stdin)
+
+func init() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(buf, maxBufSize)
+}
+
+func main() {
+	X := nextInt()
+	var ans int
+	dep := 100
+	for {
+		dep += dep / 100
+		ans++
+		if dep >= X {
+			break
+		}
+	}
+	fmt.Println(ans)
+}
+
+func nextInt() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
