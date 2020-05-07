74249ef4205e0f348dec234cf8d83ffd6f8ffec3
Yutaro Suzuki <yutaro1985@gmail.com>
2020-05-07 23:47:43 +0900

    素因数分解したmapを返す関数を追加

--- Samples/funcs.go
+++ Samples/funcs.go
@@ -221,3 +221,18 @@ func NextPermutation(x sort.Interface) bool {
 	}
 	return true
 }
+
+// 素因数分解したmapを返す
+func primeFuctorize(n int) map[int]int {
+	pf := map[int]int{}
+	for i := 2; i*i <= n; i++ {
+		for n%i == 0 {
+			pf[i]++
+			n /= i
+		}
+	}
+	if n != 1 {
+		pf[n]++
+	}
+	return pf
+}
