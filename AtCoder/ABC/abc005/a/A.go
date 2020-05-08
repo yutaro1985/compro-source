046760b6e95982b30f33a1fae4e79e4c4f9d03e2
Yutaro Suzuki <yutaro1985@gmail.com>
2020-05-09 07:36:37 +0900

    abc005 A~C AC

---
+++ AtCoder/ABC/abc005/a/A.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x, y int
+	fmt.Scan(&x, &y)
+	fmt.Println(y / x)
+}
---
+++ AtCoder/ABC/abc005/c/C.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 1e4
+	maxBufSize     = 1e6 + 7
+)
+
+var buf []byte = make([]byte, maxBufSize)
+var sc = bufio.NewScanner(os.Stdin)
+
+func init() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(buf, maxBufSize)
+}
+
+func main() {
+	T, N := nextInt(), nextInt()
+	An := make([]int, N)
+	for i := range An {
+		An[i] = nextInt()
+	}
+	M := nextInt()
+	Bn := make([]int, M)
+	for i := range Bn {
+		Bn[i] = nextInt()
+	}
+	ans := "yes"
+	if M > N {
+		ans = "no"
+	} else {
+		j := 0
+		for i := 0; i < N; i++ {
+			if An[i]+T >= Bn[j] && An[i] <= Bn[j] {
+				j++
+			}
+			if j == len(Bn) {
+				ans = "yes"
+				break
+			}
+			ans = "no"
+		}
+	}
+	fmt.Println(ans)
+}
+
+func nextLine() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
