718beebb1d578e8ec1e2e1df41f912860ce42cf3
Yutaro Suzuki <yutaro1985@gmail.com>
2020-05-28 23:24:54 +0900

    abc107 A,B AC

---
+++ AtCoder/ABC/abc107/b/B.go
@@ -0,0 +1,171 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+const (
+	initialBufSize = 1e4
+	maxBufSize     = 1e6 + 7
+)
+
+var buf []byte = make([]byte, maxBufSize)
+var sc = bufio.NewScanner(os.Stdin)
+
+func init() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(buf, maxBufSize)
+}
+
+func main() {
+	H, W := nextInt(), nextInt()
+	HW := make([][]string, H)
+	isAllWhiteRow := make([]bool, H)
+	isAllWhiteCol := make([]bool, W)
+	for i := 0; i < H; i++ {
+		row := nextLine()
+		if !strings.Contains(row, "#") {
+			isAllWhiteRow[i] = true
+		}
+		HW[i] = strings.Split(row, "")
+	}
+	for i := 0; i < W; i++ {
+		isAllWhiteCol[i] = true
+		for j := 0; j < H; j++ {
+			if HW[j][i] == "#" {
+				isAllWhiteCol[i] = false
+				break
+			}
+		}
+	}
+	for i := 0; i < H; i++ {
+		if isAllWhiteRow[i] {
+			continue
+		}
+		for j := 0; j < W; j++ {
+			if !isAllWhiteCol[j] {
+				fmt.Printf("%s", HW[i][j])
+			}
+			if j == W-1 {
+				fmt.Println()
+			}
+		}
+	}
+}
+
+func nextLine() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func abs(a int) int {
+	if a > 0 {
+		return a
+	} else {
+		return -a
+	}
+}
+
+func pow(p, q int) int {
+	res := p
+	if q == 0 {
+		return 1
+	}
+	for i := 0; i < q-1; i++ {
+		res *= p
+	}
+	return res
+}
+
+func MinOf(vars ...int) int {
+	min := vars[0]
+	for _, i := range vars {
+		if min > i {
+			min = i
+		}
+	}
+	return min
+}
+
+func MaxOf(vars ...int) int {
+	max := vars[0]
+	for _, i := range vars {
+		if max < i {
+			max = i
+		}
+	}
+	return max
+}
+
+func gcdof2numbers(a int, b int) int {
+	if b == 0 {
+		return a
+	}
+	return gcdof2numbers(b, a%b)
+}
+
+func lcmof2numbers(a int, b int) int {
+	return a / gcdof2numbers(a, b) * b
+}
+
+// マイナスの場合は考慮していない
+func fuctorial(a int) int {
+	if a == 1 || a == 0 {
+		return 1
+	} else {
+		return fuctorial(a-1) * a
+	}
+}
+
+func NextPermutation(x sort.Interface) bool {
+	n := x.Len() - 1
+	if n < 1 {
+		return false
+	}
+	j := n - 1
+	for ; !x.Less(j, j+1); j-- {
+		if j == 0 {
+			return false
+		}
+	}
+	l := n
+	for !x.Less(j, l) {
+		l--
+	}
+	x.Swap(j, l)
+	for k, l := j+1, n; k < l; {
+		x.Swap(k, l)
+		k++
+		l--
+	}
+	return true
+}
+
+// 素因数分解したmapを返す
+func primeFuctorize(n int) map[int]int {
+	pf := map[int]int{}
+	for i := 2; i*i <= n; i++ {
+		for n%i == 0 {
+			pf[i]++
+			n /= i
+		}
+	}
+	if n != 1 {
+		pf[n]++
+	}
+	return pf
+}
