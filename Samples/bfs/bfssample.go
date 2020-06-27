aa458d622679eb7bbc9b48becf6e71e1ad061937
Yutaro Suzuki <yutaro1985@gmail.com>
2020-06-28 05:45:01 +0900

    サンプル更新

---
+++ Samples/bfs/bfssample.go
@@ -0,0 +1,1 @@
+../../AtCoder/ABC/abc168/d/D.go
--- Samples/template2.go
+++ Samples/template2.go
@@ -137,3 +137,49 @@ func primeFuctorize(n int) map[int]int {
 	}
 	return pf
 }
+
+// ここから拝借
+// https://shoman.hatenablog.com/entry/2020/02/25/185456
+// Stackは[]intのエイリアス
+type Stack []int
+
+// Push adds an element
+func (s *Stack) Push(v int) {
+	*s = append(*s, v)
+}
+
+// Pop removes the top element and return it
+func (s *Stack) Pop() (int, error) {
+	if s.Empty() {
+		return 0, fmt.Errorf("stack is empty")
+	}
+
+	v := (*s)[len(*s)-1]
+	*s = (*s)[:len(*s)-1]
+	return v, nil
+}
+
+// Peek returns the top value
+func (s *Stack) Peek() (int, error) {
+	if s.Empty() {
+		return 0, fmt.Errorf("stack is empty")
+	}
+
+	return (*s)[len(*s)-1], nil
+}
+
+// Size returns the length of stack
+func (s *Stack) Size() int {
+	return len(*s)
+}
+
+// Empty returns true when stack is empty
+func (s *Stack) Empty() bool {
+	return s.Size() == 0
+}
+
+// NewStack generates stack
+func NewStack() *Stack {
+	s := new(Stack)
+	return s
+}
