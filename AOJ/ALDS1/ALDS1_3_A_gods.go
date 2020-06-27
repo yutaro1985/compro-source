595c21e680af109e09470d8d82097f94c8bb8349
Yutaro Suzuki <yutaro1985@gmail.com>
2020-06-28 04:44:53 +0900

    AOJでは通らない実装

---
+++ AOJ/ALDS1/ALDS1_3_A_gods.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+
+	lls "github.com/emirpasic/gods/stacks/linkedliststack"
+)
+
+// AOJではgodsが使えないので通せない
+
+func main() {
+	stack := lls.New()
+
+	sc := bufio.NewScanner(os.Stdin)
+	sc.Scan()
+	formula := strings.Split(sc.Text(), " ")
+	for _, v := range formula {
+		switch v {
+		case "+":
+			a, _ := stack.Pop()
+			b, _ := stack.Pop()
+			stack.Push(a.(int) + b.(int))
+		case "-":
+			a, _ := stack.Pop()
+			b, _ := stack.Pop()
+			stack.Push(a.(int) - b.(int))
+		case "*":
+			a, _ := stack.Pop()
+			b, _ := stack.Pop()
+			stack.Push(a.(int) * b.(int))
+		default:
+			num, _ := strconv.Atoi(v)
+			stack.Push(num)
+		}
+	}
+	ans, _ := stack.Pop()
+	fmt.Println(ans)
+}
