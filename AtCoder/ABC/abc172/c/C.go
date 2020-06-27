39261b10fd2bbff8c23d619f9e3b524ba1723745
Yutaro Suzuki <yutaro1985@gmail.com>
2020-06-28 02:23:11 +0900

    abc172 C AC 実装で無駄に詰まった

---
+++ AtCoder/ABC/abc172/c/C.go
@@ -0,0 +1,169 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+)
+
+const (
+	initialBufSize = 1e4
+	maxBufSize     = 1e6 + 7
+)
+
+var buf []byte = make([]byte, maxBufSize)
+var sc = bufio.NewScanner(os.Stdin)
+
+func init() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(buf, maxBufSize)
+}
+
+func main() {
+	N, M, K := nextInt(), nextInt(), nextInt()
+	An := make([]int, N)
+	Bm := make([]int, M)
+	CsumA := make([]uint, N+1)
+	CsumB := make([]uint, M+1)
+	var ans int
+	for i := 0; i < N; i++ {
+		An[i] = nextInt()
+		CsumA[i+1] = CsumA[i] + uint(An[i])
+	}
+	for i := 0; i < M; i++ {
+		Bm[i] = nextInt()
+		CsumB[i+1] = CsumB[i] + uint(Bm[i])
+	}
+	// 二分探索する
+	for i := 0; i <= N; i++ {
+		if CsumA[i] > uint(K) {
+			break
+		}
+		ok := -1
+		ng := M + 1
+		for abs(ng-ok) > 1 {
+			mid := (ok + ng) / 2
+			if uint(K) >= CsumA[i]+CsumB[mid] {
+				ok = mid
+			} else {
+				ng = mid
+			}
+			ans = MaxOf(ans, i+ok)
+		}
+	}
+	fmt.Println(ans)
+}
+
+func nextLine() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func abs(a int) int {
+	if a > 0 {
+		return a
+	} else {
+		return -a
+	}
+}
+
+func pow(p, q int) int {
+	res := p
+	if q == 0 {
+		return 1
+	}
+	for i := 0; i < q-1; i++ {
+		res *= p
+	}
+	return res
+}
+
+func MinOf(vars ...int) int {
+	min := vars[0]
+	for _, i := range vars {
+		if min > i {
+			min = i
+		}
+	}
+	return min
+}
+
+func MaxOf(vars ...int) int {
+	max := vars[0]
+	for _, i := range vars {
+		if max < i {
+			max = i
+		}
+	}
+	return max
+}
+
+func gcdof2numbers(a int, b int) int {
+	if b == 0 {
+		return a
+	}
+	return gcdof2numbers(b, a%b)
+}
+
+func lcmof2numbers(a int, b int) int {
+	return a / gcdof2numbers(a, b) * b
+}
+
+// マイナスの場合は考慮していない
+func fuctorial(a int) int {
+	if a == 1 || a == 0 {
+		return 1
+	} else {
+		return fuctorial(a-1) * a
+	}
+}
+
+func NextPermutation(x sort.Interface) bool {
+	n := x.Len() - 1
+	if n < 1 {
+		return false
+	}
+	j := n - 1
+	for ; !x.Less(j, j+1); j-- {
+		if j == 0 {
+			return false
+		}
+	}
+	l := n
+	for !x.Less(j, l) {
+		l--
+	}
+	x.Swap(j, l)
+	for k, l := j+1, n; k < l; {
+		x.Swap(k, l)
+		k++
+		l--
+	}
+	return true
+}
+
+// 素因数分解したmapを返す
+func primeFuctorize(n int) map[int]int {
+	pf := map[int]int{}
+	for i := 2; i*i <= n; i++ {
+		for n%i == 0 {
+			pf[i]++
+			n /= i
+		}
+	}
+	if n != 1 {
+		pf[n]++
+	}
+	return pf
+}
