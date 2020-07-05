095227390d446a8377d3244eb3281259f3477dfc
Yutaro Suzuki <yutaro1985@gmail.com>
2020-07-06 05:52:16 +0900

    ヒープのサンプルがサジェスト汚染してたので修正

--- Samples/template2.go
+++ Samples/template2.go
@@ -188,39 +188,24 @@ func NewStack() *Stack {
 // golangの公式サンプルより
 // https://xn--go-hh0g6u.com/pkg/container/heap/#example__intHeap
 // IntHeap は，整数の最小ヒープです。
-type IntHeap []int
+type IH []int
 
-func (h IntHeap) Len() int           { return len(h) }
-func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
-func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+func (h IH) Len() int           { return len(h) }
+func (h IH) Less(i, j int) bool { return h[i] < h[j] }
+func (h IH) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
 
-func (h *IntHeap) Push(x interface{}) {
+func (h *IH) Push(x interface{}) {
 	// Push と Pop はポインタレシーバを使っています。
 	// なぜなら，スライスの中身だけでなく，スライスの長さも変更するからです。
 	*h = append(*h, x.(int))
 }
 
-func (h *IntHeap) Pop() interface{} {
+func (h *IH) Pop() interface{} {
 	old := *h
 	n := len(old)
 	x := old[n-1]
 	*h = old[0 : n-1]
 	return x
-}
-
-// この例では IntHeap にいくつかの整数を挿入しています。そして，最小値をチェックしてから，
-// 優先度順に取り除いています。
-func Example_intHeap() {
-	h := &IntHeap{2, 1, 5}
-	heap.Init(h)
-	heap.Push(h, 3)
-	fmt.Printf("minimum: %d\n", (*h)[0])
-	for h.Len() > 0 {
-		fmt.Printf("%d ", heap.Pop(h))
-	}
-	// Output:
-	// minimum: 1
-	// 1 2 3 5
 }
 
 // golangの公式サンプルより
