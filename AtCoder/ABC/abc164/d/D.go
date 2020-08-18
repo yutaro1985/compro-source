02163ea179d11b0d224f755cb81e0211e29b975f
Yutaro Suzuki <yutaro1985@gmail.com>
2020-08-19 04:21:43 +0900

    abc164 D AC 解説を少しは理解した

--- AtCoder/ABC/abc164/d/D.go
+++ AtCoder/ABC/abc164/d/D.go
@@ -2,20 +2,28 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
+
+// 解法はこちらを参考
+// https://drken1215.hatenablog.com/entry/2020/04/29/171300
 
 func main() {
 	var S string
 	var ans int
 	fmt.Scan(&S)
-	for i := 4; i < len(S); i++ {
-		for j := 0; i+j <= len(S); j++ {
-			check, _ := strconv.Atoi(S[j : i+j])
-			if check%2019 == 0 {
-				ans++
-			}
-		}
+	val := make([]int, 2019)
+	digit := 1
+	cur := 0
+	val[cur]++
+	N := len(S)
+	for i := 0; i < N; i++ {
+		add := int(S[N-1-i] - '0')
+		cur = (cur + digit*add) % 2019
+		digit = (digit * 10) % 2019
+		val[cur]++
+	}
+	for _, v := range val {
+		ans += v * (v - 1) / 2
 	}
 	fmt.Println(ans)
 }
