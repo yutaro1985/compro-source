5a9388f29157eefdc38f3485f70ba783e4bd5d33
Yutaro Suzuki <yutaro1985@gmail.com>
2020-09-04 05:52:10 +0900

    usa_k法を使った素因数分解を追加

--- Samples/template2.go
+++ Samples/template2.go
@@ -137,8 +137,8 @@ func NextPermutation(x sort.Interface) bool {
 	return true
 }
 
-// 素因数分解したmapを返す
-func primeFuctorize(n int) map[int]int {
+// PrimeFactorize 素因数分解したmapを返す
+func PrimeFactorize(n int) map[int]int {
 	pf := map[int]int{}
 	for i := 2; i*i <= n; i++ {
 		for n%i == 0 {
@@ -150,6 +150,37 @@ func primeFuctorize(n int) map[int]int {
 		pf[n]++
 	}
 	return pf
+}
+
+// FastPrimeFactorize osa_k法を使った高速素因数分解
+// sieveを作るところのjはi*2スタートでもよい？
+// https://qiita.com/rsk0315_h4x/items/ff3b542a4468679fb409
+func FastPrimeFactorize(n int, sieve []int) map[int]int {
+	pf := make(map[int]int)
+	for n > 1 {
+		pf[sieve[n]]++
+		n /= sieve[n]
+	}
+	return pf
+}
+
+// MakeSieve は高速素数判定に使う篩を作る。
+// Sieve[i] < i なら合成数なので、Sieveは素数判定にも使える
+func MakeSieve(max int) []int {
+	sieve := make([]int, max)
+	for i := range sieve {
+		sieve[i] = i
+	}
+	for i := 2; i*i <= max; i++ {
+		if sieve[i] == i {
+			for j := i; j < max; j += i {
+				if sieve[j] == j {
+					sieve[j] = i
+				}
+			}
+		}
+	}
+	return sieve
 }
 
 // golangの公式サンプルより
