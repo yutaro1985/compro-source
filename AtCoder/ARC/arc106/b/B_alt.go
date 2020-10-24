56aab0e3fd1c9ecb36d476149ded3a3edd930ee0
Yutaro Suzuki <yutaro1985@gmail.com>
2020-10-25 01:32:06 +0900

    解説を参考にして別海

---
+++ AtCoder/ARC/arc106/b/B_alt.go
@@ -0,0 +1,229 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"reflect"
+	"strconv"
+)
+
+const (
+	initialBufSize = 1e4
+	maxBufSize     = 1e6 + 7
+)
+
+var buf []byte = make([]byte, initialBufSize)
+var sc = bufio.NewScanner(os.Stdin)
+
+func init() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(buf, maxBufSize)
+}
+
+// 問題によって値は調整する
+const (
+	mod     = int(1e9) + 7
+	maxsize = 510000
+)
+
+func main() {
+	N, M := nextInt(), nextInt()
+	a, b := make([]int, N), make([]int, N)
+	for i := 0; i < N; i++ {
+		a[i] = nextInt()
+	}
+	for i := 0; i < N; i++ {
+		b[i] = nextInt()
+	}
+	uf := newUnionFind(N)
+	for i := 0; i < M; i++ {
+		uf.unite(nextInt()-1, nextInt()-1)
+	}
+	for i := 0; i < N; i++ {
+		root := uf.find(i)
+		if root != i {
+			a[root] += a[i]
+			b[root] += b[i]
+			a[i] = 0
+			b[i] = 0
+		}
+	}
+	if reflect.DeepEqual(a, b) {
+		fmt.Println("Yes")
+	} else {
+		fmt.Println("No")
+	}
+}
+
+type Edge struct {
+	c int
+	d int
+}
+
+func nextLine() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+// Math Utilities
+// https://play.golang.org/p/bm7uZi0zCN
+
+// Abs Absolute Value
+func Abs(a int) int {
+	if a > 0 {
+		return a
+	}
+	return -a
+}
+
+// Pow Integer power: compute a**b using binary powering algorithm
+// See Donald Knuth, The Art of Computer Programming, Volume 2, Section 4.6.3
+func Pow(a, b int) int {
+	p := 1
+	for b > 0 {
+		if b&1 != 0 {
+			p *= a
+		}
+		b >>= 1
+		a *= a
+	}
+	return p
+}
+
+// PowMod Modular integer power: compute a**b mod m using binary powering algorithm
+func PowMod(a, b, m int) int {
+	a = a % m
+	p := 1 % m
+	for b > 0 {
+		if b&1 != 0 {
+			p = (p * a) % m
+		}
+		b >>= 1
+		a = (a * a) % m
+	}
+	return p
+}
+
+func ceil(a, b int) int {
+	return (a + (b - 1)) / b
+}
+
+// MinOf 与えられたintのうち最小のものを返す
+func MinOf(vars ...int) int {
+	min := vars[0]
+	for _, i := range vars {
+		if min > i {
+			min = i
+		}
+	}
+	return min
+}
+
+// MaxOf 与えられたintのうち最大のものを返す
+func MaxOf(vars ...int) int {
+	max := vars[0]
+	for _, i := range vars {
+		if max < i {
+			max = i
+		}
+	}
+	return max
+}
+
+// ChminInt 第一引数のほうが大きかった場合第二引数の値を代入する。
+// 1つ目の値は参照渡しする
+func ChminInt(a *int, b int) {
+	if *a > b {
+		*a = b
+	}
+}
+
+// ChmanInt 第一引数のほうが小さかった場合第二引数の値を代入する。
+// 1つ目の値は参照渡しする
+func ChmanInt(a *int, b int) {
+	if *a < b {
+		*a = b
+	}
+}
+
+// UnionFind の定義
+// 下記を参考
+// https://youtu.be/TdR816rqc3s?t=6822
+// https://github.com/atcoder/live_library/blob/master/uf.cpp
+// https://qiita.com/haru1843/items/2295d0ec1f5002bd5c33#%E5%AE%9F%E8%A3%85
+type UnionFind struct {
+	parent []int
+	maxlen int
+}
+
+// UnionFind のスライス初期化
+func newUnionFind(N int) *UnionFind {
+	u := new(UnionFind)
+	u.parent = make([]int, N)
+	u.maxlen = 1
+	for i := range u.parent {
+		u.parent[i] = -1
+	}
+	return u
+}
+
+// xの根を見つける
+func (u *UnionFind) find(x int) int {
+	if u.parent[x] < 0 {
+		return x
+	}
+	u.parent[x] = u.find(u.parent[x])
+	return u.parent[x]
+}
+
+// xとyのグループを結合する
+func (u *UnionFind) unite(x, y int) {
+	xf := u.find(x)
+	yf := u.find(y)
+	if xf == yf {
+		return
+	}
+	// 常に大きい方に小さい木をくっつける
+	if xf > yf {
+		xf, yf = yf, xf
+	}
+	u.parent[xf] += u.parent[yf]
+	u.parent[yf] = xf
+	if u.parent[xf] < 0 {
+		u.maxlen = MaxOf(u.maxlen, Abs(u.parent[xf]))
+	}
+}
+
+// xとyが同じグループに所属するかどうかを返す
+func (u *UnionFind) same(x, y int) bool {
+	if u.find(x) == u.find(y) {
+		return true
+	}
+	return false
+}
+
+// xの所属するグループの木の大きさを返す
+func (u *UnionFind) size(x int) int {
+	return -u.parent[u.find(x)]
+}
+
+// 根の数を返す
+func (u UnionFind) rootcnt() int {
+	var cnt int
+	for _, v := range u.parent {
+		if v < 0 {
+			cnt++
+		}
+	}
+	return cnt
+}
