f05e479a2d893fabe11b0edfb7a2b56f758b40c4
Yutaro Suzuki <yutaro1985@gmail.com>
2020-10-24 23:08:58 +0900

    arc106 A,B AC

---
+++ AtCoder/ARC/arc106/a/A.go
@@ -0,0 +1,151 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 1e4
+	maxBufSize     = 1e6 + 7
+)
+
+var buf []byte = make([]byte, initialBufSize)
+var sc = bufio.NewScanner(os.Stdin)
+
+func init() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(buf, maxBufSize)
+}
+
+// 問題によって値は調整する
+const (
+	mod     = int(1e9) + 7
+	maxsize = 510000
+)
+
+func main() {
+	N := nextInt()
+	memo3 := make([]int, 40)
+	memo5 := make([]int, 33)
+	memo3[1] = 3
+	memo5[1] = 5
+	for i := 2; i <= 39; i++ {
+		memo3[i] = memo3[i-1] * 3
+	}
+	for i := 2; i <= 32; i++ {
+		memo5[i] = memo5[i-1] * 5
+	}
+	for i := 1; i < len(memo3); i++ {
+		if memo3[i] >= int(1e18) {
+			break
+		}
+		for j := 1; j < len(memo5); j++ {
+			if memo5[j] >= int(1e18) {
+				break
+			}
+			if memo3[i]+memo5[j] == N {
+				fmt.Println(i, j)
+				return
+			}
+		}
+	}
+	fmt.Println(-1)
+}
+
+func nextLine() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+// Math Utilities
+// https://play.golang.org/p/bm7uZi0zCN
+
+// Abs Absolute Value
+func Abs(a int) int {
+	if a > 0 {
+		return a
+	}
+	return -a
+}
+
+// Pow Integer power: compute a**b using binary powering algorithm
+// See Donald Knuth, The Art of Computer Programming, Volume 2, Section 4.6.3
+func Pow(a, b int) int {
+	p := 1
+	for b > 0 {
+		if b&1 != 0 {
+			p *= a
+		}
+		b >>= 1
+		a *= a
+	}
+	return p
+}
+
+// PowMod Modular integer power: compute a**b mod m using binary powering algorithm
+func PowMod(a, b, m int) int {
+	a = a % m
+	p := 1 % m
+	for b > 0 {
+		if b&1 != 0 {
+			p = (p * a) % m
+		}
+		b >>= 1
+		a = (a * a) % m
+	}
+	return p
+}
+
+func ceil(a, b int) int {
+	return (a + (b - 1)) / b
+}
+
+// MinOf 与えられたintのうち最小のものを返す
+func MinOf(vars ...int) int {
+	min := vars[0]
+	for _, i := range vars {
+		if min > i {
+			min = i
+		}
+	}
+	return min
+}
+
+// MaxOf 与えられたintのうち最大のものを返す
+func MaxOf(vars ...int) int {
+	max := vars[0]
+	for _, i := range vars {
+		if max < i {
+			max = i
+		}
+	}
+	return max
+}
+
+// ChminInt 第一引数のほうが大きかった場合第二引数の値を代入する。
+// 1つ目の値は参照渡しする
+func ChminInt(a *int, b int) {
+	if *a > b {
+		*a = b
+	}
+}
+
+// ChmanInt 第一引数のほうが小さかった場合第二引数の値を代入する。
+// 1つ目の値は参照渡しする
+func ChmanInt(a *int, b int) {
+	if *a < b {
+		*a = b
+	}
+}
