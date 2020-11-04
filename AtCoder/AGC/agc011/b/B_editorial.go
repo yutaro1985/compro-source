95908a827467646c21cbefe57698fc9597b2541b
Yutaro Suzuki <yutaro1985@gmail.com>
2020-11-05 02:53:30 +0900

    agc011 B 解説の方法でAC

---
+++ AtCoder/AGC/agc011/b/B_editorial.go
@@ -0,0 +1,229 @@
+package main
+
+import (
+	"bufio"
+	"container/heap"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+)
+
+const (
+	initialBufSize = 1e4
+	maxBufSize     = 1e6 + 7
+)
+
+var buf []byte = make([]byte, initialBufSize)
+var sc = bufio.NewScanner(os.Stdin)
+
+func init() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(buf, maxBufSize)
+}
+
+// 問題によって値は調整する
+const (
+	mod     = int(1e9) + 7
+	maxsize = 510000
+)
+
+func main() {
+	N := nextInt()
+	A := newInts(N)
+	sort.Ints(A)
+	Csum := make([]int, N+1)
+	for i := 0; i < N; i++ {
+		Csum[i+1] = Csum[i] + A[i]
+	}
+	var ans int
+	for i := N - 1; i >= 0; i-- {
+		if A[i] > (Csum[i]-Csum[0])*2 {
+			ans = N - i
+			break
+		}
+	}
+	fmt.Println(ans)
+}
+
+func nextLine() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func newInts(N int) []int {
+	res := make([]int, N)
+	for i := range res {
+		res[i] = nextInt()
+	}
+	return res
+}
+
+func newStrings(N int) []string {
+	res := make([]string, N)
+	for i := range res {
+		res[i] = nextLine()
+	}
+	return res
+}
+
+// Math Utilities
+// https://play.golang.org/p/bm7uZi0zCN
+
+// Abs Absolute Value
+func Abs(a int) int {
+	if a > 0 {
+		return a
+	}
+	return -a
+}
+
+// Pow Integer power: compute a**b using binary powering algorithm
+// See Donald Knuth, The Art of Computer Programming, Volume 2, Section 4.6.3
+func Pow(a, b int) int {
+	p := 1
+	for b > 0 {
+		if b&1 != 0 {
+			p *= a
+		}
+		b >>= 1
+		a *= a
+	}
+	return p
+}
+
+// PowMod Modular integer power: compute a**b mod m using binary powering algorithm
+func PowMod(a, b, m int) int {
+	a = a % m
+	p := 1 % m
+	for b > 0 {
+		if b&1 != 0 {
+			p = (p * a) % m
+		}
+		b >>= 1
+		a = (a * a) % m
+	}
+	return p
+}
+
+func ceil(a, b int) int {
+	return (a + (b - 1)) / b
+}
+
+// MinOf 与えられたintのうち最小のものを返す
+func MinOf(vars ...int) int {
+	min := vars[0]
+	for _, i := range vars {
+		if min > i {
+			min = i
+		}
+	}
+	return min
+}
+
+// MaxOf 与えられたintのうち最大のものを返す
+func MaxOf(vars ...int) int {
+	max := vars[0]
+	for _, i := range vars {
+		if max < i {
+			max = i
+		}
+	}
+	return max
+}
+
+// ChminInt 第一引数のほうが大きかった場合第二引数の値を代入する。
+// 1つ目の値は参照渡しする
+func ChminInt(a *int, b int) {
+	if *a > b {
+		*a = b
+	}
+}
+
+// ChmaxInt 第一引数のほうが小さかった場合第二引数の値を代入する。
+// 1つ目の値は参照渡しする
+func ChmaxInt(a *int, b int) {
+	if *a < b {
+		*a = b
+	}
+}
+
+// IH golangの公式サンプルより
+// https://xn--go-hh0g6u.com/pkg/container/heap/#example__intHeap
+type IH []int
+
+func (h IH) Len() int { return len(h) }
+
+func (h IH) Less(i, j int) bool { return h[i] < h[j] }
+func (h IH) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *IH) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+func (h *IH) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+// golangの公式サンプルより
+// https://xn--go-hh0g6u.com/pkg/container/heap/#example__priorityQueue
+
+// Item は，優先キューで管理する項目です。
+type Item struct {
+	value    int // 値。任意です。
+	priority int // キューにおける優先度
+	// index は heap.Interface メソッドで更新されます。
+	index int // ヒープにおけるインデックス
+}
+
+// PriorityQueue は heap.Interface を実装し，Item のリストを保持します。
+type PriorityQueue []*Item
+
+func (pq PriorityQueue) Len() int { return len(pq) }
+
+func (pq PriorityQueue) Less(i, j int) bool {
+	return pq[i].priority < pq[j].priority
+}
+
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].index = i
+	pq[j].index = j
+}
+
+func (pq *PriorityQueue) Push(x interface{}) {
+	n := len(*pq)
+	item := x.(*Item)
+	item.index = n
+	*pq = append(*pq, item)
+}
+
+func (pq *PriorityQueue) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	old[n-1] = nil  // メモリリークを避ける
+	item.index = -1 // 安全のため
+	*pq = old[0 : n-1]
+	return item
+}
+
+// update はキューの Item の優先度と値を更新します。
+func (pq *PriorityQueue) update(item *Item, value int, priority int) {
+	item.value = value
+	item.priority = priority
+	heap.Fix(pq, item.index)
+}
