33a80d854ffe98692fb4beac95427b1b9386bf96
Yutaro Suzuki <yutaro1985@gmail.com>
2020-11-05 04:22:55 +0900

    abc156 D AC modintのテストも兼ねて

---
+++ AtCoder/ABC/abc156/d/D.go
@@ -0,0 +1,223 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 1e4
+	maxBufSize     = 1e6 + 7
+)
+
+var buf []byte = make([]byte, initialBufSize)
+var sc = bufio.NewScanner(os.Stdin)
+
+func init() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(buf, maxBufSize)
+}
+
+// 問題によって値は調整する
+const (
+	mod     = int(1e9) + 7
+	maxsize = 500100
+)
+
+// 参考
+// https://drken1215.hatenablog.com/entry/2020/04/22/013400
+// nCk= n*(n-1)*…*(n-k+2)*(n-k+1)/(k*(k-1)*…*2*1)ということを利用するとO(K)で計算できてしまう。
+
+func main() {
+	n, a, b := nextInt(), nextInt(), nextInt()
+	ans := mint(2).pow(mint(n)).sub(mint(1))
+	ans = ans.sub(calcnCk(n, a)).sub(calcnCk(n, b))
+	fmt.Println(ans)
+}
+
+func calcnCk(N, K int) mint {
+	res := mint(1)
+	for i := 0; i < K; i++ {
+		res.mulSelf(mint(N - i))
+		res.divSelf(mint(i + 1))
+	}
+	return res
+}
+
+func nextLine() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+// Math Utilities
+// https://play.golang.org/p/bm7uZi0zCN
+
+// Abs Absolute Value
+func Abs(a int) int {
+	if a > 0 {
+		return a
+	}
+	return -a
+}
+
+// Pow Integer power: compute a**b using binary powering algorithm
+// See Donald Knuth, The Art of Computer Programming, Volume 2, Section 4.6.3
+func Pow(a, b int) int {
+	p := 1
+	for b > 0 {
+		if b&1 != 0 {
+			p *= a
+		}
+		b >>= 1
+		a *= a
+	}
+	return p
+}
+
+// PowMod Modular integer power: compute a**b mod m using binary powering algorithm
+func PowMod(a, b, m int) int {
+	a = a % m
+	p := 1 % m
+	for b > 0 {
+		if b&1 != 0 {
+			p = (p * a) % m
+		}
+		b >>= 1
+		a = (a * a) % m
+	}
+	return p
+}
+
+func ceil(a, b int) int {
+	return (a + (b - 1)) / b
+}
+
+// MinOf 与えられたintのうち最小のものを返す
+func MinOf(vars ...int) int {
+	min := vars[0]
+	for _, i := range vars {
+		if min > i {
+			min = i
+		}
+	}
+	return min
+}
+
+// MaxOf 与えられたintのうち最大のものを返す
+func MaxOf(vars ...int) int {
+	max := vars[0]
+	for _, i := range vars {
+		if max < i {
+			max = i
+		}
+	}
+	return max
+}
+
+type mint int64
+
+func (mi mint) mod() mint {
+	mi %= mint(mod)
+	if mi < 0 {
+		mi += mint(mod)
+	}
+	return mi
+}
+func (mi mint) inv() mint {
+	// https://qiita.com/drken/items/3b4fdf0a78e7a138cd9a#3-5-%E6%8B%A1%E5%BC%B5-euclid-%E3%81%AE%E4%BA%92%E9%99%A4%E6%B3%95%E3%81%AB%E3%82%88%E3%82%8B%E9%80%86%E5%85%83%E8%A8%88%E7%AE%97
+	b, u, v := mint(mod), mint(1), mint(0)
+	for b != 0 {
+		t := mi / b
+		mi -= t * b
+		mi, b = b, mi
+		u -= t * v
+		u, v = v, u
+	}
+	u %= mint(mod)
+	if u < 0 {
+		u += mint(mod)
+	}
+	return u
+	// return mi.pow(mint(0).sub(2))
+}
+func (mi mint) pow(n mint) mint {
+	p := mint(1)
+	for n > 0 {
+		if n&1 == 1 {
+			p.mulSelf(mi)
+		}
+		mi.mulSelf(mi)
+		n >>= 1
+	}
+	return p
+}
+func (mi mint) add(n mint) mint {
+	return (mi + n).mod()
+}
+func (mi mint) sub(n mint) mint {
+	return (mi - n).mod()
+}
+func (mi mint) mul(n mint) mint {
+	return (mi * n).mod()
+}
+func (mi mint) div(n mint) mint {
+	return mi.mul(n.inv())
+}
+func (mi *mint) addSelf(n mint) *mint {
+	*mi = mi.add(n)
+	return mi
+}
+func (mi *mint) subSelf(n mint) *mint {
+	*mi = mi.sub(n)
+	return mi
+}
+func (mi *mint) mulSelf(n mint) *mint {
+	*mi = mi.mul(n)
+	return mi
+}
+func (mi *mint) divSelf(n mint) *mint {
+	*mi = mi.div(n)
+	return mi
+}
+
+// COMで使うスライス
+var fac = make([]int, maxsize)
+var finv = make([]int, maxsize)
+var inv = make([]int, maxsize)
+
+// COMinit で COMで使うためのテーブルを作る前処理
+// O(N)
+// https://qiita.com/drken/items/3b4fdf0a78e7a138cd9a#5-%E4%BA%8C%E9%A0%85%E4%BF%82%E6%95%B0-ncr
+func COMinit() {
+	fac[0], fac[1] = 1, 1
+	finv[0], finv[1] = 1, 1
+	inv[1] = 1
+	for i := 2; i < maxsize; i++ {
+		fac[i] = fac[i-1] * i % mod
+		inv[i] = mod - inv[mod%i]*(mod/i)%mod
+		finv[i] = finv[i-1] * inv[i] % mod
+	}
+}
+
+// COM nCkを求める。COMinitを先に実行する
+// COMinitの結果を使ってO(1)で行える
+func COM(n, k int) int {
+	if n < k {
+		return 0
+	}
+	if n < 0 || k < 0 {
+		return 0
+	}
+	return fac[n] * (finv[k] * finv[n-k] % mod) % mod
+}
