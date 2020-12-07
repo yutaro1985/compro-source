d18b01c6c0a6d4103c7de85eda9e778784c250f4
Yutaro Suzuki <yutaro1985@gmail.com>
2020-12-08 04:14:52 +0900

    ARC110 A~C AC Cは解説を参考

---
+++ AtCoder/ARC/arc110/c/C_editorial.go
@@ -0,0 +1,222 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"reflect"
+	"strconv"
+)
+
+const (
+	initialBufSize = 1e4
+	maxBufSize     = 1e6 + 7
+)
+
+var buf []byte = make([]byte, initialBufSize)
+var sc = bufio.NewScanner(os.Stdin)
+
+func init() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(buf, maxBufSize)
+}
+
+// 問題によって値は調整する
+const (
+	mod     = int(1e9) + 7
+	maxsize = 510000
+)
+
+func main() {
+	N := nextInt()
+	P := make([]int, N)
+	indexes := make(map[int]int)
+	// used := make([]bool, N)
+	ans := make([]int, 0)
+	check := make([]int, N)
+	for i := 0; i < N; i++ {
+		check[i] = i + 1
+	}
+	for i := 0; i < N; i++ {
+		P[i] = nextInt()
+		indexes[P[i]] = i
+	}
+	// fmt.Println(P, indexes)
+	if indexes[N+1] == N || indexes[1] == 0 {
+		fmt.Println(-1)
+		return
+	}
+	var min int
+	for i := 1; i < N; {
+		memo := indexes[i]
+		if indexes[i] <= min {
+			fmt.Println(-1)
+			return
+		}
+		for indexes[i] > min {
+			index := indexes[i]
+			P[index], P[index-1] = P[index-1], P[index]
+			indexes[i] = index - 1
+			indexes[P[index]] = index
+			ans = append(ans, index)
+			// fmt.Println(P, indexes)
+		}
+		i = memo + 1
+		min = memo
+		// fmt.Println(i, min)
+	}
+	if reflect.DeepEqual(check, P) {
+		for _, v := range ans {
+			fmt.Println(v)
+		}
+	} else {
+		fmt.Println(-1)
+	}
+}
+
+func nextLine() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func makeInts(N int) []int {
+	res := make([]int, N)
+	for i := range res {
+		res[i] = nextInt()
+	}
+	return res
+}
+
+func makeStrings(N int) []string {
+	res := make([]string, N)
+	for i := range res {
+		res[i] = nextLine()
+	}
+	return res
+}
+
+func init2DInts(H, W, N int) [][]int {
+	res := make([][]int, H)
+	for i := 0; i < H; i++ {
+		res[i] = make([]int, W)
+		if N != 0 {
+			for j := 0; j < W; j++ {
+				res[i][j] = N
+			}
+		}
+	}
+	return res
+}
+
+func init2DBools(H, W int, b bool) [][]bool {
+	res := make([][]bool, H)
+	for i := 0; i < H; i++ {
+		res[i] = make([]bool, W)
+		if b {
+			for j := 0; j < W; j++ {
+				res[i][j] = true
+			}
+		}
+	}
+	return res
+}
+
+// Math Utilities
+// https://play.golang.org/p/bm7uZi0zCN
+
+// Abs Absolute Value
+func Abs(a int) int {
+	if a > 0 {
+		return a
+	}
+	return -a
+}
+
+// Pow Integer power: compute a**b using binary powering algorithm
+// See Donald Knuth, The Art of Computer Programming, Volume 2, Section 4.6.3
+func Pow(a, b int) int {
+	p := 1
+	for b > 0 {
+		if b&1 != 0 {
+			p *= a
+		}
+		b >>= 1
+		a *= a
+	}
+	return p
+}
+
+// PowMod Modular integer power: compute a**b mod m using binary powering algorithm
+func PowMod(a, b, m int) int {
+	a = a % m
+	p := 1 % m
+	for b > 0 {
+		if b&1 != 0 {
+			p = (p * a) % m
+		}
+		b >>= 1
+		a = (a * a) % m
+	}
+	return p
+}
+
+// Ceil a/bを切り上げたものを返す
+func Ceil(a, b int) int {
+	return (a + (b - 1)) / b
+}
+
+// Mod 負の場合を考慮してあまりを取る
+func Mod(a, m int) int {
+	res := a % m
+	if res < 0 {
+		res += m
+	}
+	return res
+}
+
+// MinOf 与えられたintのうち最小のものを返す
+func MinOf(vars ...int) int {
+	min := vars[0]
+	for _, i := range vars {
+		if min > i {
+			min = i
+		}
+	}
+	return min
+}
+
+// MaxOf 与えられたintのうち最大のものを返す
+func MaxOf(vars ...int) int {
+	max := vars[0]
+	for _, i := range vars {
+		if max < i {
+			max = i
+		}
+	}
+	return max
+}
+
+// ChminInt 第一引数のほうが大きかった場合第二引数の値を代入する。
+// 1つ目の値は参照渡しする
+func ChminInt(a *int, b int) {
+	if *a > b {
+		*a = b
+	}
+}
+
+// ChmaxInt 第一引数のほうが小さかった場合第二引数の値を代入する。
+// 1つ目の値は参照渡しする
+func ChmaxInt(a *int, b int) {
+	if *a < b {
+		*a = b
+	}
+}
