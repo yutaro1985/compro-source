b3426ef2cafc644de7c6f9c1e6c45bb0fb5735b8
Yutaro Suzuki <yutaro1985@gmail.com>
2020-12-13 23:33:46 +0900

    abc185 A~D AC

---
+++ AtCoder/ABC/abc185/c/C.go
@@ -0,0 +1,224 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 1e4
+	maxBufSize     = 1e6 + 7
+)
+
+var buf []byte = make([]byte, initialBufSize)
+var sc = bufio.NewScanner(os.Stdin)
+
+func init() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(buf, maxBufSize)
+}
+
+// 問題によって値は調整する
+const (
+	mod     = 1 << 60
+	maxsize = 510000
+	INF     = 1 << 60
+)
+
+func main() {
+	L := nextInt()
+	ans := L - 1
+	for i := 2; i <= 11; i++ {
+		ans = ans * (L - i) / i
+	}
+	fmt.Println(ans)
+}
+
+func nextLine() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+// マイナスの場合は考慮していない
+func factorialmod(a, m int) int {
+	if a == 1 || a == 0 {
+		return 1
+	} else {
+		return (factorialmod(a-1, m) * a) % m
+	}
+}
+
+func makeInts(N int) []int {
+	res := make([]int, N)
+	for i := range res {
+		res[i] = nextInt()
+	}
+	return res
+}
+
+func makeStrings(N int) []string {
+	res := make([]string, N)
+	for i := range res {
+		res[i] = nextLine()
+	}
+	return res
+}
+
+func init2DInts(H, W, N int) [][]int {
+	res := make([][]int, H)
+	for i := 0; i < H; i++ {
+		res[i] = make([]int, W)
+		if N != 0 {
+			for j := 0; j < W; j++ {
+				res[i][j] = N
+			}
+		}
+	}
+	return res
+}
+
+func init2DBools(H, W int, b bool) [][]bool {
+	res := make([][]bool, H)
+	for i := 0; i < H; i++ {
+		res[i] = make([]bool, W)
+		if b {
+			for j := 0; j < W; j++ {
+				res[i][j] = true
+			}
+		}
+	}
+	return res
+}
+
+// Math Utilities
+// https://play.golang.org/p/bm7uZi0zCN
+
+// Abs Absolute Value
+func Abs(a int) int {
+	if a > 0 {
+		return a
+	}
+	return -a
+}
+
+// Pow Integer power: compute a**b using binary powering algorithm
+// See Donald Knuth, The Art of Computer Programming, Volume 2, Section 4.6.3
+func Pow(a, b int) int {
+	p := 1
+	for b > 0 {
+		if b&1 != 0 {
+			p *= a
+		}
+		b >>= 1
+		a *= a
+	}
+	return p
+}
+
+// PowMod Modular integer power: compute a**b mod m using binary powering algorithm
+func PowMod(a, b, m int) int {
+	a = a % m
+	p := 1 % m
+	for b > 0 {
+		if b&1 != 0 {
+			p = (p * a) % m
+		}
+		b >>= 1
+		a = (a * a) % m
+	}
+	return p
+}
+
+// Ceil a/bを切り上げたものを返す
+func Ceil(a, b int) int {
+	return (a + (b - 1)) / b
+}
+
+// Mod 負の場合を考慮してあまりを取る
+func Mod(a, m int) int {
+	res := a % m
+	if res < 0 {
+		res += m
+	}
+	return res
+}
+
+// MinOf 与えられたintのうち最小のものを返す
+func MinOf(vars ...int) int {
+	min := vars[0]
+	for _, i := range vars {
+		if min > i {
+			min = i
+		}
+	}
+	return min
+}
+
+// MaxOf 与えられたintのうち最大のものを返す
+func MaxOf(vars ...int) int {
+	max := vars[0]
+	for _, i := range vars {
+		if max < i {
+			max = i
+		}
+	}
+	return max
+}
+
+// ChminInt 第一引数のほうが大きかった場合第二引数の値を代入する。
+// 1つ目の値は参照渡しする
+func ChminInt(a *int, b int) {
+	if *a > b {
+		*a = b
+	}
+}
+
+// ChmaxInt 第一引数のほうが小さかった場合第二引数の値を代入する。
+// 1つ目の値は参照渡しする
+func ChmaxInt(a *int, b int) {
+	if *a < b {
+		*a = b
+	}
+}
+
+// COMで使うスライス
+var fac = make([]int, maxsize)
+var finv = make([]int, maxsize)
+var inv = make([]int, maxsize)
+
+// COMinit で COMで使うためのテーブルを作る前処理
+// O(N)
+// https://qiita.com/drken/items/3b4fdf0a78e7a138cd9a#5-%E4%BA%8C%E9%A0%85%E4%BF%82%E6%95%B0-ncr
+func COMinit() {
+	fac[0], fac[1] = 1, 1
+	finv[0], finv[1] = 1, 1
+	inv[1] = 1
+	for i := 2; i < maxsize; i++ {
+		fac[i] = fac[i-1] * i % mod
+		inv[i] = mod - inv[mod%i]*(mod/i)%mod
+		finv[i] = finv[i-1] * inv[i] % mod
+	}
+}
+
+// COM nCkを求める。COMinitを先に実行する
+// COMinitの結果を使ってO(1)で行える
+func COM(n, k int) int {
+	if n < k {
+		return 0
+	}
+	if n < 0 || k < 0 {
+		return 0
+	}
+	return fac[n] * (finv[k] * finv[n-k] % mod) % mod
+}
