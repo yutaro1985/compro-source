ea7050dcb2545f35b344b0161ce638d6bb3af286
Yutaro Suzuki <yutaro1985@gmail.com>
2021-02-22 03:05:08 +0900

    abc192 E AC ダイクストラ法の実装

---
+++ AtCoder/ABC/abc192/e/E.go
@@ -0,0 +1,270 @@
+package main
+
+import (
+	"bufio"
+	"container/heap"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+)
+
+const (
+	initialBufSize = 1e4
+	maxBufSize     = 1e6 + 7
+)
+
+var buf []byte = make([]byte, initialBufSize)
+var sc = bufio.NewScanner(os.Stdin)
+
+func init() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(buf, maxBufSize)
+}
+
+// 問題によって値は調整する
+const (
+	mod     = int(1e9) + 7
+	maxsize = 510000
+	INF     = 1 << 60
+)
+
+type Train struct {
+	to, T, K int
+}
+
+// ダイクストラ法
+
+func main() {
+	N, M, X, Y := nextInt(), nextInt(), nextInt()-1, nextInt()-1
+	G := make([][]Train, N)
+	for i := 0; i < M; i++ {
+		A, B, T, K := nextInt()-1, nextInt()-1, nextInt(), nextInt()
+		G[A] = append(G[A], Train{B, T, K})
+		G[B] = append(G[B], Train{A, T, K})
+	}
+	dist := initInts(N, INF)
+	pq := make(PQ, 0)
+	heap.Init(&pq)
+	dist[X] = 0
+	push := func(v, d int) {
+		if dist[v] <= d {
+			return
+		}
+		dist[v] = d
+		heap.Push(&pq, &Item{v, d, 0})
+	}
+	heap.Push(&pq, &Item{X, 0, 0})
+	for pq.Len() != 0 {
+		r := heap.Pop(&pq).(*Item)
+		if dist[r.to] != r.priority {
+			continue
+		}
+		for _, v := range G[r.to] {
+			cost := (v.K-dist[r.to]%v.K)%v.K + v.T
+			push(v.to, r.priority+cost)
+		}
+	}
+	ans := dist[Y]
+	if ans == INF {
+		fmt.Println(-1)
+	} else {
+		fmt.Println(ans)
+	}
+}
+
+type Item struct {
+	to, priority, index int
+}
+
+type PQ []*Item
+
+func (pq PQ) Len() int {
+	return len(pq)
+}
+
+func (pq PQ) Less(i, j int) bool {
+	return pq[i].priority < pq[j].priority
+}
+
+func (pq PQ) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].index = i
+	pq[j].index = j
+}
+
+func (pq *PQ) Push(x interface{}) {
+	n := len(*pq)
+	item := x.(*Item)
+	item.index = n
+	*pq = append(*pq, item)
+}
+
+func (pq *PQ) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	item.index = -1
+	*pq = old[0 : n-1]
+	return item
+}
+
+func nextLine() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func makeInts(N int) []int {
+	res := make([]int, N)
+	for i := range res {
+		res[i] = nextInt()
+	}
+	return res
+}
+
+func makeStrings(N int) []string {
+	res := make([]string, N)
+	for i := range res {
+		res[i] = nextLine()
+	}
+	return res
+}
+
+func initInts(N, I int) []int {
+	res := make([]int, N)
+	for i := 0; i < N; i++ {
+		res[i] = I
+	}
+	return res
+}
+
+func init2DInts(H, W, N int) [][]int {
+	res := make([][]int, H)
+	for i := 0; i < H; i++ {
+		res[i] = make([]int, W)
+		if N != 0 {
+			for j := 0; j < W; j++ {
+				res[i][j] = N
+			}
+		}
+	}
+	return res
+}
+
+func init2DBools(H, W int, b bool) [][]bool {
+	res := make([][]bool, H)
+	for i := 0; i < H; i++ {
+		res[i] = make([]bool, W)
+		if b {
+			for j := 0; j < W; j++ {
+				res[i][j] = true
+			}
+		}
+	}
+	return res
+}
+
+// ReverseSort はsort.Interfaceの形を渡すと逆順にソートする
+func ReverseSort(data sort.Interface) {
+	sort.Sort(sort.Reverse(data))
+}
+
+// Math Utilities
+// https://play.golang.org/p/bm7uZi0zCN
+
+// Abs Absolute Value
+func Abs(a int) int {
+	if a > 0 {
+		return a
+	}
+	return -a
+}
+
+// Pow Integer power: compute a**b using binary powering algorithm
+// See Donald Knuth, The Art of Computer Programming, Volume 2, Section 4.6.3
+func Pow(a, b int) int {
+	p := 1
+	for b > 0 {
+		if b&1 != 0 {
+			p *= a
+		}
+		b >>= 1
+		a *= a
+	}
+	return p
+}
+
+// PowMod Modular integer power: compute a**b mod m using binary powering algorithm
+func PowMod(a, b, m int) int {
+	a = a % m
+	p := 1 % m
+	for b > 0 {
+		if b&1 != 0 {
+			p = (p * a) % m
+		}
+		b >>= 1
+		a = (a * a) % m
+	}
+	return p
+}
+
+// Ceil a/bを切り上げたものを返す
+func Ceil(a, b int) int {
+	return (a + (b - 1)) / b
+}
+
+// Mod 負の場合を考慮してあまりを取る
+func Mod(a, m int) int {
+	res := a % m
+	if res < 0 {
+		res += m
+	}
+	return res
+}
+
+// MinOf 与えられたintのうち最小のものを返す
+func MinOf(vars ...int) int {
+	min := vars[0]
+	for _, i := range vars {
+		if min > i {
+			min = i
+		}
+	}
+	return min
+}
+
+// MaxOf 与えられたintのうち最大のものを返す
+func MaxOf(vars ...int) int {
+	max := vars[0]
+	for _, i := range vars {
+		if max < i {
+			max = i
+		}
+	}
+	return max
+}
+
+// ChminInt 第一引数のほうが大きかった場合第二引数の値を代入する。
+// 1つ目の値は参照渡しする
+func ChminInt(a *int, b int) {
+	if *a > b {
+		*a = b
+	}
+}
+
+// ChmaxInt 第一引数のほうが小さかった場合第二引数の値を代入する。
+// 1つ目の値は参照渡しする
+func ChmaxInt(a *int, b int) {
+	if *a < b {
+		*a = b
+	}
+}
