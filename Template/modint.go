f7f2a4675fbaffc493caf673391a2b7b512b3a6f
Yutaro Suzuki <yutaro1985@gmail.com>
2021-06-14 08:31:45 +0900

    modintに高速な二項係数のライブラリ追加

--- Template/modint.go
+++ Template/modint.go
@@ -77,7 +77,7 @@ func (mi *mint) divSelf(n mint) *mint {
 
 // SingleCOM はmodintを使ってO(K)で一つの二項係数を求める関数
 func SingleCOM(N, K int) mint {
-	if K < 0 || K > N {
+	if N < 0 || K < 0 || K > N {
 		return 0
 	}
 	res := mint(1)
@@ -86,4 +86,35 @@ func SingleCOM(N, K int) mint {
 		res.divSelf(mint(i + 1))
 	}
 	return res
+}
+
+// modCOMで使うスライス
+var fac = make([]mint, maxsize)
+var finv = make([]mint, maxsize)
+var inv = make([]mint, maxsize)
+
+// COMinit で COMで使うためのテーブルを作る前処理
+// O(N)
+// https://qiita.com/drken/items/3b4fdf0a78e7a138cd9a#5-%E4%BA%8C%E9%A0%85%E4%BF%82%E6%95%B0-ncr
+func ModCOMinit() {
+	fac[0], fac[1] = 1, 1
+	finv[0], finv[1] = 1, 1
+	inv[1] = 1
+	for i := 2; i < maxsize; i++ {
+		fac[i] = fac[i-1].mul(mint(i))
+		inv[i] = mint(mod) - inv[mod%i].mul(mint(mod/i))
+		finv[i] = finv[i-1].mul(inv[i])
+	}
+}
+
+// FascModCOM nCkを求める。COMinitを先に実行する
+// COMinitの結果を使ってO(1)で行える
+func FastModCOM(n, k int) mint {
+	if n < k {
+		return 0
+	}
+	if n < 0 || k < 0 {
+		return 0
+	}
+	return fac[n].mul(finv[k].mul(finv[n-k]))
 }
