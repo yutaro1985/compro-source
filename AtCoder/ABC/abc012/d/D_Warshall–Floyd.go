6033860692b50c7d294b67c6224d9ddbc9d61077
Yutaro Suzuki <yutaro1985@gmail.com>
2021-07-05 06:06:52 +0900

    色々

--- AtCoder/ABC/abc012/d/D_Warshall–Floyd.go
+++ AtCoder/ABC/abc012/d/D_Warshall–Floyd.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"container/heap"
 	"fmt"
 	"os"
 	"sort"
@@ -238,57 +237,6 @@ func (h *IH) Pop() interface{} {
 	x := old[n-1]
 	*h = old[0 : n-1]
 	return x
-}
-
-// golangの公式サンプルより
-// https://xn--go-hh0g6u.com/pkg/container/heap/#example__priorityQueue
-
-// Item は，優先キューで管理する項目です。
-type Item struct {
-	value    string // 値。任意です。
-	priority int    // キューにおける優先度
-	// index は heap.Interface メソッドで更新されます。
-	index int // ヒープにおけるインデックス
-}
-
-// PriorityQueue は heap.Interface を実装し，Item のリストを保持します。
-type PriorityQueue []*Item
-
-func (pq PriorityQueue) Len() int { return len(pq) }
-
-func (pq PriorityQueue) Less(i, j int) bool {
-	// Pop が最小ではなく最大の優先度を持つ項目を返して欲しいので，ここでは > を使っています。
-	return pq[i].priority > pq[j].priority
-}
-
-func (pq PriorityQueue) Swap(i, j int) {
-	pq[i], pq[j] = pq[j], pq[i]
-	pq[i].index = i
-	pq[j].index = j
-}
-
-func (pq *PriorityQueue) Push(x interface{}) {
-	n := len(*pq)
-	item := x.(*Item)
-	item.index = n
-	*pq = append(*pq, item)
-}
-
-func (pq *PriorityQueue) Pop() interface{} {
-	old := *pq
-	n := len(old)
-	item := old[n-1]
-	old[n-1] = nil  // メモリリークを避ける
-	item.index = -1 // 安全のため
-	*pq = old[0 : n-1]
-	return item
-}
-
-// update はキューの Item の優先度と値を更新します。
-func (pq *PriorityQueue) update(item *Item, value string, priority int) {
-	item.value = value
-	item.priority = priority
-	heap.Fix(pq, item.index)
 }
 
 // UnionFind の定義
