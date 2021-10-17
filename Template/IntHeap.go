9520fdd53712c18a753fcf7c5668b580c7bc8d02
Yutaro Suzuki <yutaro1985@gmail.com>
2021-10-18 03:11:03 +0900

    ライブラリ改善とテスト

--- Template/IntHeap.go
+++ Template/IntHeap.go
@@ -2,9 +2,16 @@
 // https://xn--go-hh0g6u.com/pkg/container/heap/#example__intHeap
 type IH []int
 
+func InitIH() IH {
+	ih := make(IH, 0)
+	heap.Init(&ih)
+	return ih
+}
+
 func (h IH) Len() int { return len(h) }
 
-func (h IH) Less(i, j int) bool { return h[i] < h[j] }
+// 不等号の向きは都度調整する（デフォルトでは降順）
+func (h IH) Less(i, j int) bool { return h[i] > h[j] }
 func (h IH) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
 
 func (h *IH) Push(x interface{}) {
@@ -23,7 +30,7 @@ func (h *IH) Pop() interface{} {
 	return x
 }
 
-func (h *IH) HPop() interface{} {
+func (h *IH) HPop() int {
 	x := heap.Pop(h).(int)
 	return x
 }
--- Template/PriorityQueue.go
+++ Template/PriorityQueue.go
@@ -4,6 +4,12 @@ type Item struct {
 
 type PQ []*Item
 
+func InitPQ() PQ {
+	pq := make(PQ, 0)
+	heap.Init(&pq)
+	return pq
+}
+
 func (pq PQ) Len() int {
 	return len(pq)
 }
@@ -25,11 +31,20 @@ func (pq *PQ) Push(x interface{}) {
 	*pq = append(*pq, item)
 }
 
+func (pq *PQ) HPush(x Item) {
+	heap.Push(pq, &x)
+}
+
 func (pq *PQ) Pop() interface{} {
 	old := *pq
 	n := len(old)
 	item := old[n-1]
 	item.index = -1
 	*pq = old[0 : n-1]
+	return item
+}
+
+func (pq *PQ) HPop() *Item {
+	item := heap.Pop(pq).(*Item)
 	return item
 }
