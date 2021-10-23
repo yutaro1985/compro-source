e5724c8729cc295bcc95b9b143f472d473408448
Yutaro Suzuki <yutaro1985@gmail.com>
2021-10-24 03:55:16 +0900

    abc224 d 解説AC

---
+++ AtCoder/ABC/abc224/d/D.go
@@ -0,0 +1,304 @@
+package main
+
+import (
+	"bufio"
+	"container/list"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+const (
+	initialBufSize = 1e4
+	maxBufSize     = 1e6 + 7
+)
+
+var buf []byte = make([]byte, initialBufSize)
+var sc = bufio.NewScanner(os.Stdin)
+
+func init() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(buf, maxBufSize)
+}
+
+// 問題によって値は調整する
+const (
+	mod     = int(1e9) + 7
+	maxsize = 510000
+	INF     = 1 << 60
+)
+
+type Order struct {
+	a, b, c, d, e, f, g, h, i int
+}
+
+func main() {
+	M := nextInt()
+	G := make([][]int, 10)
+	for i := 0; i < M; i++ {
+		u, v := nextInt(), nextInt()
+		G[u] = append(G[u], v)
+		G[v] = append(G[v], u)
+	}
+	p := initInts(9, 9)
+	for i := 0; i < 8; i++ {
+		pi := nextInt()
+		p[pi-1] = i + 1
+	}
+	toS := func(p []int) string {
+		var res string
+		tmp := make([]string, 0)
+		for _, v := range p {
+			tmp = append(tmp, strconv.Itoa(v))
+		}
+		res = strings.Join(tmp, "")
+		return res
+	}
+	s := toS(p)
+	dist := make(map[string]int)
+	dist[s] = 0
+	q := list.New()
+	q.PushBack(s)
+	for q.Len() > 0 {
+		pre := q.Front().Value.(string)
+		q.Remove(q.Front())
+		var v int
+		for i := 0; i < 9; i++ {
+			if pre[i] == '9' {
+				v = i + 1
+			}
+		}
+		for _, nv := range G[v] {
+			t := []byte(pre)
+			t[v-1], t[nv-1] = t[nv-1], t[v-1]
+			next := string(t)
+			if _, e := dist[next]; !e {
+				dist[next] = dist[pre] + 1
+				q.PushBack(next)
+			}
+		}
+	}
+	if ans, e := dist["123456789"]; e {
+		fmt.Println(ans)
+	} else {
+		fmt.Println(-1)
+	}
+}
+
+// マイナスの場合は考慮していない
+func factorialmod(a, m int) int {
+	if a == 1 || a == 0 {
+		return 1
+	} else {
+		return (factorialmod(a-1, m) * a) % m
+	}
+}
+
+// NextPermutation はsortインターフェース（要はソート済みスライス）から次の順列にスライスを並べ替える
+// https://play.golang.org/p/ljft9xhOEn
+func NextPermutation(x sort.Interface) bool {
+	n := x.Len() - 1
+	if n < 1 {
+		return false
+	}
+	j := n - 1
+	for ; !x.Less(j, j+1); j-- {
+		if j == 0 {
+			return false
+		}
+	}
+	l := n
+	for !x.Less(j, l) {
+		l--
+	}
+	x.Swap(j, l)
+	for k, l := j+1, n; k < l; {
+		x.Swap(k, l)
+		k++
+		l--
+	}
+	return true
+}
+
+func nextLine() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func nextFloat64() float64 {
+	sc.Scan()
+	f, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return f
+}
+
+func nextComplex128() complex128 {
+	return complex(nextFloat64(), nextFloat64())
+}
+
+func makeInts(N int) []int {
+	res := make([]int, N)
+	for i := range res {
+		res[i] = nextInt()
+	}
+	return res
+}
+
+func makeStrings(N int) []string {
+	res := make([]string, N)
+	for i := range res {
+		res[i] = nextLine()
+	}
+	return res
+}
+
+func initInts(N, I int) []int {
+	res := make([]int, N)
+	for i := 0; i < N; i++ {
+		res[i] = I
+	}
+	return res
+}
+
+func init2DInts(H, W, N int) [][]int {
+	res := make([][]int, H)
+	for i := 0; i < H; i++ {
+		res[i] = make([]int, W)
+		if N != 0 {
+			for j := 0; j < W; j++ {
+				res[i][j] = N
+			}
+		}
+	}
+	return res
+}
+
+func init2DBools(H, W int, b bool) [][]bool {
+	res := make([][]bool, H)
+	for i := 0; i < H; i++ {
+		res[i] = make([]bool, W)
+		if b {
+			for j := 0; j < W; j++ {
+				res[i][j] = true
+			}
+		}
+	}
+	return res
+}
+
+// LocalPrint はisLocalが1のときだけ標準出力するデバッグ用関数
+func LocalPrint(i ...interface{}) {
+	if os.Getenv("isLocal") == "1" {
+		fmt.Printf("%s", "Local: ")
+		fmt.Println(i...)
+	}
+}
+
+// ReverseSort はsort.Interfaceの形を渡すと逆順にソートする
+func ReverseSort(data sort.Interface) {
+	sort.Sort(sort.Reverse(data))
+}
+
+// Math Utilities
+// https://play.golang.org/p/bm7uZi0zCN
+
+// Abs Absolute Value
+func Abs(a int) int {
+	if a > 0 {
+		return a
+	}
+	return -a
+}
+
+// Pow Integer power: compute a**b using binary powering algorithm
+// See Donald Knuth, The Art of Computer Programming, Volume 2, Section 4.6.3
+func Pow(a, b int) int {
+	p := 1
+	for b > 0 {
+		if b&1 != 0 {
+			p *= a
+		}
+		b >>= 1
+		a *= a
+	}
+	return p
+}
+
+// PowMod Modular integer power: compute a**b mod m using binary powering algorithm
+func PowMod(a, b, m int) int {
+	a = a % m
+	p := 1 % m
+	for b > 0 {
+		if b&1 != 0 {
+			p = (p * a) % m
+		}
+		b >>= 1
+		a = (a * a) % m
+	}
+	return p
+}
+
+// Ceil a/bを切り上げたものを返す
+func Ceil(a, b int) int {
+	return (a + (b - 1)) / b
+}
+
+// Mod 負の場合を考慮してあまりを取る
+func Mod(a, m int) int {
+	res := a % m
+	if res < 0 {
+		res += m
+	}
+	return res
+}
+
+// MinOf 与えられたintのうち最小のものを返す
+func MinOf(vars ...int) int {
+	min := vars[0]
+	for _, i := range vars {
+		if min > i {
+			min = i
+		}
+	}
+	return min
+}
+
+// MaxOf 与えられたintのうち最大のものを返す
+func MaxOf(vars ...int) int {
+	max := vars[0]
+	for _, i := range vars {
+		if max < i {
+			max = i
+		}
+	}
+	return max
+}
+
+// ChminInt 第一引数のほうが大きかった場合第二引数の値を代入する。
+// 1つ目の値は参照渡しする
+func ChminInt(a *int, b int) {
+	if *a > b {
+		*a = b
+	}
+}
+
+// ChmaxInt 第一引数のほうが小さかった場合第二引数の値を代入する。
+// 1つ目の値は参照渡しする
+func ChmaxInt(a *int, b int) {
+	if *a < b {
+		*a = b
+	}
+}
